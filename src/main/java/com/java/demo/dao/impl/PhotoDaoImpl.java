c40ea4c8f1f7b03155c99a9b63cd14bdc44c1cd1
buerguoo <buerguoo@163.com>
2021-08-03 17:56:12 +0800

    default

--- src/main/java/com/java/demo/dao/impl/PhotoDaoImpl.java
+++ src/main/java/com/java/demo/dao/impl/PhotoDaoImpl.java
@@ -2,10 +2,7 @@ package com.java.demo.dao.impl;
 
 import java.util.Collection;
 
-<<<<<<< HEAD
-=======
 import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> dev
 import org.springframework.stereotype.Repository;
 
 import com.java.demo.dao.PhotoDao;
@@ -15,13 +12,9 @@ import com.java.demo.model.entity.Photo;
 @Repository
 public class PhotoDaoImpl implements PhotoDao {
 
-<<<<<<< HEAD
-	
-=======
 	@Autowired
 	private PhotoMapper photoMapper;
 
->>>>>>> dev
 	@Override
 	public void insert(Photo entity) {
 
