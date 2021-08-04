7d7c0313a0d002175f74eefc29b7cbe06ec39c80
buerguoo <buerguoo@163.com>
2021-08-04 13:49:05 +0800

    wrapper

---
+++ src/main/java/com/java/demo/model/utils/PageWrapper.java
@@ -0,0 +1,17 @@
+package com.java.demo.model.utils;
+
+import lombok.Data;
+
+@Data
+public class PageWrapper {
+
+    private int pageSize;
+
+    private int start;
+
+    public PageWrapper(int pageSize, int page) {
+        this.pageSize = pageSize;
+        this.start = page * pageSize;
+    }
+
+}
