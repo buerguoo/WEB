29d7b77d7c1be2fbb29803efe46f7f301a47ecb3
IcGan <dynastyg@163.com>
2021-08-05 11:43:45 +0800

    sdfsdf

--- src/main/java/com/java/demo/controller/IndexController.java
+++ src/main/java/com/java/demo/controller/IndexController.java
@@ -16,6 +16,33 @@ public class IndexController {
 	@Autowired
 	private UserService userService;
 	
+	
+	/*
+	 浏览器打开任意网站，f12，切到console页面，输入以下代码（对于/login/UserLogin请求）测试是否跨域成功
+
+	
+	------code------
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'http://127.0.0.1:8080/login/UserLogin?email=admin&password=123456');
+	xhr.send(null);
+	xhr.onload = function(e) {
+    var xhr = e.target;
+    console.log(xhr.responseText);
+    }
+    ------code------
+    
+	-------跨域失败的输出------
+	 
+	 Access to XMLHttpRequest at 'http://127.0.0.1:8080/login/UserLogin?email=admin&password=123456' 
+	 from origin 'https://www.cnblogs.com' has been blocked by CORS policy: 
+	 No 'Access-Control-Allow-Origin' header is present on the requested resource.
+	 
+	 ------跨域成功的输出（前提是建好数据库）------
+	 
+	 {"code":"1010","message":"success","data":{"id":1,"name":"主人","account":"admin","password":"123"}}
+	 
+	 */
+	//加@CrossOrigin注解实现跨域
 	@CrossOrigin
 	@RequestMapping("/login/UserLogin")
 	public ResponseWrapper getEntity(@RequestParam("email") String email,
