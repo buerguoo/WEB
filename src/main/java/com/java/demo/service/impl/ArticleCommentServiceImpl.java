d2e18e995d7c0959130a993199e68f6b27030e7a
IcGan <dynastyg@163.com>
2021-08-07 00:07:46 +0800

    dev

--- src/main/java/com/java/demo/service/impl/ArticleCommentServiceImpl.java
+++ src/main/java/com/java/demo/service/impl/ArticleCommentServiceImpl.java
@@ -1,5 +1,6 @@
 package com.java.demo.service.impl;
 
+import java.sql.Timestamp;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,6 @@ import org.springframework.stereotype.Service;
 import com.java.demo.dao.ArticleCommentDao;
 import com.java.demo.model.entity.ArticleComment;
 import com.java.demo.service.ArticleCommentService;
-import com.sun.jmx.snmp.Timestamp;
 
 @Service("ArticleComment")
 public class ArticleCommentServiceImpl implements ArticleCommentService {
@@ -37,34 +37,45 @@ public class ArticleCommentServiceImpl implements ArticleCommentService {
 	}
 
 	@Override
-	public void replyArticleCommentById(int commentId,int artId,int userId, int replyId,String content,Timestamp posttime) {
-		
-		int id = -1;
-		int replyId = 0;
+	public void replyArticleCommentById(Integer artId,Integer userId, Integer replyId,String content,Timestamp posttime) {
+
+		ArticleComment articleComment = null;
+//		int id = -1;
+//		int replyId = 0;
 //		if(username==null)
 //			username = "匿名用户";
-		ArticleComment articleComment = null;
-		//读出文章对应评论
-//		Collection<ArticleComment> articleComments = articlecommentdao.getAllArticleComments(articleId);
-//		for( ArticleComment ac :articleComments)
-//		{
-//			if(ac.getFloor()>floor)
-//				floor = ac.getFloor();
-//		}
-//		floor = floor+1;
-		articleComments = articlecommentdao.getAll();
-		for( ArticleComment ac:articleComments)
-		{
-			if(ac.getArtId()>id)
-				id = ac.getArtId();
-		}
-		id = id+1;
-		
-		long t1 = System.currentTimeMillis();
-		java.sql.Timestamp tp = new java.sql.Timestamp(t1);
-		articleComment =new ArticleComment(commentid,artId,userId,replyId,content,posttime);
-		articleComment = new ArticleComment(id,articleId,floor,replyfloor,content,username,tp);
+
+		Integer commentId=articlecommentdao.getMaxCommentId()+1;
+		articleComment =new ArticleComment(commentId, artId, userId, replyId, content, posttime);
 		articlecommentdao.insert(articleComment);
+//		//读出文章对应评论
+////		Collection<ArticleComment> articleComments = articlecommentdao.getAllArticleComments(articleId);
+////		for( ArticleComment ac :articleComments)
+////		{
+////			if(ac.getFloor()>floor)
+////				floor = ac.getFloor();
+////		}
+////		floor = floor+1;
+//		articleComments = articlecommentdao.getAll();
+//		for( ArticleComment ac:articleComments)
+//		{
+//			if(ac.getArtId()>id)
+//				id = ac.getArtId();
+//		}
+//		id = id+1;
+//		
+//		long t1 = System.currentTimeMillis();
+//		java.sql.Timestamp tp = new java.sql.Timestamp(t1);
+
+//		articleComment = new ArticleComment(id,articleId,floor,replyfloor,content,username,tp);
+//		articlecommentdao.insert(articleComment);
 	}
+
+	@Override
+	public int getMaxCommentId() {
+		// TODO Auto-generated method stub
+		return articlecommentdao.getMaxCommentId();
+	}
+
 
 }
