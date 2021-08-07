1d30b82aecb24c054c4a01b230fbb83bf71f1586
buerguoo <buerguoo@163.com>
2021-08-07 12:46:24 +0800

    articleComment

--- src/main/java/com/java/demo/service/impl/ArticleCommentServiceImpl.java
+++ src/main/java/com/java/demo/service/impl/ArticleCommentServiceImpl.java
@@ -36,57 +36,69 @@ public class ArticleCommentServiceImpl implements ArticleCommentService {
 		// TODO Auto-generated method stub
 		articlecommentdao.delete(articleId);
 	}
-
-	@Override
-	public void replyArticleCommentById(User user, int replyId,String content,int articleId) {
-		// TODO Auto-generated method stub
-		int id = -1;
-		if(user.getUsername()==null)
-			user.setUsername("匿名用户");
-		ArticleComment articleComment = null;
-		Collection<ArticleComment> articleComments = articlecommentdao.getAllArticleComments(articleId);
-		for( ArticleComment ac :articleComments)
-		{
-			if(ac.getArtId()>id)
-				id = ac.getArtId();
-		}
-		id = id+1;
-		long t1 = System.currentTimeMillis();
-		java.sql.Timestamp tp = new java.sql.Timestamp(t1);
-		articleComment = new ArticleComment(id,articleId,user.getUserId(), replyId,content,tp);
-	}
-	public void replyArticleCommentById(Integer artId,Integer userId, Integer replyId,String content,Timestamp posttime) {
-
-		ArticleComment articleComment = null;
+//
+//	@Override
+//	public void replyArticleCommentById(User user, int replyId,String content,int articleId) {
+//		// TODO Auto-generated method stub
 //		int id = -1;
-//		int replyId = 0;
-//		if(username==null)
-//			username = "匿名用户";
-
-		Integer commentId=articlecommentdao.getMaxCommentId()+1;
-		articleComment =new ArticleComment(commentId, artId, userId, replyId, content, posttime);
-		articlecommentdao.insert(articleComment);
-//		//读出文章对应评论
-////		Collection<ArticleComment> articleComments = articlecommentdao.getAllArticleComments(articleId);
-////		for( ArticleComment ac :articleComments)
-////		{
-////			if(ac.getFloor()>floor)
-////				floor = ac.getFloor();
-////		}
-////		floor = floor+1;
-//		articleComments = articlecommentdao.getAll();
-//		for( ArticleComment ac:articleComments)
+//		if(user.getUsername()==null)
+//			user.setUsername("匿名用户");
+//		ArticleComment articleComment = null;
+//		Collection<ArticleComment> articleComments = articlecommentdao.getAllArticleComments(articleId);
+//		for( ArticleComment ac :articleComments)
 //		{
 //			if(ac.getArtId()>id)
 //				id = ac.getArtId();
 //		}
 //		id = id+1;
-//		
 //		long t1 = System.currentTimeMillis();
 //		java.sql.Timestamp tp = new java.sql.Timestamp(t1);
-
-//		articleComment = new ArticleComment(id,articleId,floor,replyfloor,content,username,tp);
+//		articleComment = new ArticleComment(id,articleId,user.getUserId(), replyId,content,tp);
+//	}
+//	public void replyArticleCommentById(Integer artId,Integer userId, Integer replyId,String content,Timestamp posttime) {
+//
+//		ArticleComment articleComment = null;
+////		int id = -1;
+////		int replyId = 0;
+////		if(username==null)
+////			username = "匿名用户";
+//
+//		Integer commentId=articlecommentdao.getMaxCommentId()+1;
+//		articleComment =new ArticleComment(commentId, artId, userId, replyId, content, posttime);
 //		articlecommentdao.insert(articleComment);
+////		//读出文章对应评论
+//////		Collection<ArticleComment> articleComments = articlecommentdao.getAllArticleComments(articleId);
+//////		for( ArticleComment ac :articleComments)
+//////		{
+//////			if(ac.getFloor()>floor)
+//////				floor = ac.getFloor();
+//////		}
+//////		floor = floor+1;
+////		articleComments = articlecommentdao.getAll();
+////		for( ArticleComment ac:articleComments)
+////		{
+////			if(ac.getArtId()>id)
+////				id = ac.getArtId();
+////		}
+////		id = id+1;
+////		
+////		long t1 = System.currentTimeMillis();
+////		java.sql.Timestamp tp = new java.sql.Timestamp(t1);
+//
+////		articleComment = new ArticleComment(id,articleId,floor,replyfloor,content,username,tp);
+////		articlecommentdao.insert(articleComment);
+//	}
+
+	@Override
+	public Integer getMaxCommentId() {
+		// TODO Auto-generated method stub
+		return articlecommentdao.getMaxCommentId();
+	}
+
+	@Override
+	public ArticleComment getArticleCommentById(Integer commentId) {
+		// TODO Auto-generated method stub
+		return articlecommentdao.search(commentId);
 	}
 
 
